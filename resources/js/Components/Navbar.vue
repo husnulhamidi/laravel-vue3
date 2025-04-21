62b1eb4299ce39696be5c1df247e79150b90f0c9
KTV-2021-139\Husnul Hamidi <nul.hamidi@gmail.com>
2025-04-21 08:33:55 +0700

    remove clone

--- resources/js/Components/Navbar.vue
+++ resources/js/Components/Navbar.vue
@@ -13,7 +13,7 @@ const logout = () => {
 <template>
         <nav class="navbar navbar-expand-lg navbar-laravelflow">
             <div class="container">
-            <a class="navbar-brand" href="/"><strong>Laravel Flow</strong>Clone</a>
+            <a class="navbar-brand" href="/"><strong>Laravel Flow</strong></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
